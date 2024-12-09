0d4f023ee82c76e16cbe9f89e42be5f250f5c6de
soydevelop <elliottmelvin969@gmail.com>
2024-12-09 11:00:23 -0700

    Tried to fix As3 but didnt work

--- src/As3_Teams/As3_LeagueMain.java
+++ src/As3_Teams/As3_LeagueMain.java
@@ -10,9 +10,11 @@ public class As3_LeagueMain {
 
         ArrayList<As3_Team> allTeams = new ArrayList<>();
         loadFile("data/NBA Teams.csv", allTeams);
-
-        for (As3_Team allTeam : allTeams) {
-            allTeam.printMe();
+//        for (As3_Team allTeam : allTeams) {
+//            allTeam.printMe();
+//        }
+        for (int i = 0; i < allTeams.size(); i++) {
+            System.out.println(allTeams.get(5).nickname);
         }
 
         while(0 == 0){
@@ -23,19 +25,20 @@ public class As3_LeagueMain {
     }
 
     public static void loadFile(String filename, ArrayList<As3_Team> list) {
-
+        int i = 0;
         try {
             BufferedReader file = new BufferedReader(new FileReader(filename));
 
             String dataToRead;
             while (file.ready()) {
+
                 dataToRead = file.readLine();
 
-                String tempArray[] = dataToRead.split(",");
+                String[] tempArray = dataToRead.split(",");
 
                 list.add(new As3_Team(tempArray[0], tempArray[1], tempArray[2], Integer.parseInt(tempArray[3]), Integer.parseInt(tempArray[4]), Integer.parseInt(tempArray[5])));
-
-
+                list.get(i).printMe();
+                i++;
             }
         } catch (IOException e) {
             System.out.println(e);
